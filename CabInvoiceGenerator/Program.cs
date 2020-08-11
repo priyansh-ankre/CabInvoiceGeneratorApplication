07b5e50859cee97f5600863e209a2f880d0b1e32
priyansh-ankre <bod123priyansh@gmail.com>
2020-08-11 20:02:13 +0530

    [Priyansh]Added:test cases for calculating total fare and minimum fare

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -6,7 +6,7 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Cab Invoice Generator");
         }
     }
 }
